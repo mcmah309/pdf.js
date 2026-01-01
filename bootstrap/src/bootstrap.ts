8d8118407f556048fc9871538a0690f4b1ff00e4
mcmah309 <dillonmcmahon79@gmail.com>
2026-01-01 02:19:00 +0000

    Add back all original html and bootstrap highlighting to work as intended

--- bootstrap/src/bootstrap.ts
+++ bootstrap/src/bootstrap.ts
@@ -1,5 +1,6 @@
 import { PdfController } from "./controller";
 import { PDFViewerApplication } from "../../web/app.js";
+import { AnnotationEditorType } from "../../src/shared/util.js";
 
 function hidePdfjsEditorModeButtons(): void {
   [
@@ -8,6 +9,10 @@ function hidePdfjsEditorModeButtons(): void {
     "#pageRotateCw",
     "#pageRotateCcw",
     "#download",
+    // ".toolbar",
+    "#documentPropertiesDialog",
+    "#altTextDialog",
+    "#printServiceDialog",
   ].forEach(item => {
     const element = document.querySelector(item) as HTMLElement;
     if (element) {
@@ -16,23 +21,71 @@ function hidePdfjsEditorModeButtons(): void {
       if (nextDiv && nextDiv.classList.contains("horizontalToolbarSeparator")) {
         nextDiv.style.display = "none";
       }
+    } else {
+      console.error(`Could not find element with selector '${item}' to hide.`);
     }
   });
 }
 
-
-
-
 export function preScript(): void {
-    hidePdfjsEditorModeButtons();
+  hidePdfjsEditorModeButtons();
 }
 
 export function postScript(): void {
-    const eventBus = PDFViewerApplication.eventBus;
-    if (eventBus) {
-        eventBus._on("documentloaded", async () => {
-            // todo bind methods
-            PdfController._isReady = true;
-        });
+  const eventBus = PDFViewerApplication.eventBus;
+  eventBus._on("documentloaded", async () => {
+    PdfController._isReady = true;
+    console.trace("PdfController READY");
+  });
+  let lastEditorMode: number = AnnotationEditorType.NONE;
+  let isCurrentlyEditing: boolean = false;
+  eventBus._on("annotationeditormodechanged", (event: any) => {
+    let mode = event?.mode;
+    if (mode == undefined) {
+      console.error("No mode in annotationeditormodechanged event");
+      return;
     }
+    lastEditorMode = mode;
+  });
+  eventBus._on("annotationeditorstateschanged", (event: any) => {
+    let isEditing = event?.details?.isEditing;
+    if (isEditing == undefined) {
+      console.error("No isEditing in annotationeditorstateschanged event");
+      return;
+    }
+    isCurrentlyEditing = isEditing;
+  });
+  const highlightButton = document.getElementById("editorHighlightButton");
+  if (highlightButton) {
+    document.addEventListener("click", event => {
+      if (lastEditorMode == AnnotationEditorType.NONE) {
+        return;
+      }
+
+      // Ignore double clicks since they open marks
+      if (event.detail > 1) {
+        return;
+      }
+
+      const target = event.target as HTMLElement;
+      if (
+        target.closest("#dialogContainer") ||
+        target.closest('[role="toolbar"]') ||
+        target.closest('[role="mark"]')
+      ) {
+        return;
+      }
+
+      if (highlightButton.getAttribute("aria-expanded") === "true") {
+        highlightButton.click();
+      }
+
+      // eventBus.dispatch("switchannotationeditormode", {
+      //   source: "bootstrap",
+      //   mode: AnnotationEditorType.NONE,
+      // });
+    });
+  } else {
+    console.error("Could not find highlight button");
+  }
 }
--- bootstrap/src/controller.ts
+++ bootstrap/src/controller.ts
@@ -1,4 +1,12 @@
+import { PDFViewerApplication } from "../../web/app.js";
+import * as pdfjsLib from "../../src/pdf.js";
+
 export class PdfController {
     static _isReady = false;
-    // eventBus
+    static _pdfViewerApplication = PDFViewerApplication;
+    static _pdfjsLib = pdfjsLib;
+    
+    static getCurrentPage(): number {
+        return this._pdfViewerApplication.page;
+    }
 }
