63a5b570bdf33b5c2ef5c0d5f729ad9d8c3bcef5
mcmah309 <dillonmcmahon79@gmail.com>
2026-01-02 00:40:13 +0000

    Text only highlighting working

--- bootstrap/src/bootstrap.ts
+++ bootstrap/src/bootstrap.ts
@@ -57,13 +57,29 @@ export function postScript(): void {
   });
   const highlightButton = document.getElementById("editorHighlightButton");
   if (highlightButton) {
-    document.addEventListener("click", event => {
+    const deactivateHighlighting = (event: Event) => {
       if (lastEditorMode == AnnotationEditorType.NONE) {
+        console.trace("Highlight mode not active; ignoring");
         return;
       }
 
+      if (highlightButton.getAttribute("aria-expanded") === "true") {
+        console.trace("Deactivating highlight mode");
+        highlightButton.click();
+      } else {
+        console.trace("Highlight button already closed");
+      }
+
+      // eventBus.dispatch("switchannotationeditormode", {
+      //   source: "bootstrap",
+      //   mode: AnnotationEditorType.NONE,
+      // });
+    };
+
+    document.addEventListener("pointerdown", event => {
       // Ignore double clicks since they open marks
       if (event.detail > 1) {
+        console.trace("Ignoring double click event");
         return;
       }
 
@@ -73,17 +89,34 @@ export function postScript(): void {
         target.closest('[role="toolbar"]') ||
         target.closest('[role="mark"]')
       ) {
+        console.trace("Click inside dialog, toolbar, or mark; ignoring");
         return;
       }
 
-      if (highlightButton.getAttribute("aria-expanded") === "true") {
-        highlightButton.click();
+      deactivateHighlighting(event);
+      // We also must fire pointer up so a user could not hold and draw
+      const myPointerUp = new PointerEvent("pointerup", {
+        bubbles: true,
+        cancelable: true,
+        pointerId: event.pointerId,
+        pointerType: event.pointerType,
+      });
+      target.dispatchEvent(myPointerUp);
+    });
+
+    document.addEventListener("keydown", event => {
+      // Ignore if typing in a text input or textarea
+      const target = event.target as HTMLElement;
+      if (
+        target.tagName === "INPUT" ||
+        target.tagName === "TEXTAREA" ||
+        target.isContentEditable
+      ) {
+        console.trace("Ignoring keydown event in editable element");
+        return;
       }
 
-      // eventBus.dispatch("switchannotationeditormode", {
-      //   source: "bootstrap",
-      //   mode: AnnotationEditorType.NONE,
-      // });
+      deactivateHighlighting(event);
     });
   } else {
     console.error("Could not find highlight button");
